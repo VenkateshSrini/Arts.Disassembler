32051bd49b9c9cd8cb9a315bdb71319f2d03855c
VENKATESH SRINIVASAN <venkateshsrini3@gmail.com>
2018-06-27 13:16:09 +0530

    Code commit without Test
    Code commit without Test

---
+++ src/Arts.DisAssembler/ILDisAssembler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ICSharpCode.Decompiler;
+using ICSharpCode.Decompiler.CSharp;
+using Mono.Cecil;
+
+namespace Arts.DisAssembler
+{
+    public class ILDisAssembler
+    {
+        public  void DecompileAsProject(string assemblyFileName, string outputDirectory)
+        {
+            ModuleDefinition module = UniversalAssemblyResolver.LoadMainModule(assemblyFileName);
+            WholeProjectDecompiler decompiler = new WholeProjectDecompiler();
+            decompiler.DecompileProject(module, outputDirectory);
+        }
+    }
+}
